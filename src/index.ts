86f9cb8569629a0e01490361696b4d755b68d163
MateCocido-js <mateorodriguezxxi@gmail.com>
2025-05-28 20:59:19 -0300

    agregué unos cambios

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,11 @@ class Product {
     this.price = price;
   }
   static findProductsBelow(price: number) {
-    const nuevoArray = products.filter((p) => {
-      p.price < price;
-    });
-    return nuevoArray;
-  }
+  const filtered = products.filter(p => p.price < price);
+  return filtered.map(p => new Product(p.name, p.price)); // Creamos instancias de Product
+}
+
+
   id: number;
   name: string;
   price: number;
@@ -26,9 +26,17 @@ class User {
     this.products.push(newProduct);
   }
   addProducts(newProducts: Product[]) {
+    for (const products of newProducts){
+      this.products.push(products)
+    }
     // esto no funciona:
     this.products.concat(newProducts);
+
     // pista: push no suma muchos items (agrega de a uno)
+  }
+
+  findAffordableProducts(price: number) {
+    return Product.findProductsBelow(price); // Llama al método estático
   }
 }
 
